d0b85df2182f806c72695e2c72105150d8d6efb4
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-09-28 17:01:42 -0500

    added commented out code to prepare for api calling

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// fetch(`api.giphy.com/v1/gifs?api_key=2k0QNVSP1By7SAWJhixukFzzhRErrBXl&ids=${gifIDs}`)
+// paulId = cmIBKv6BpofWUPPmKU
+// duncan idaho = TI4NWYIGY201td3R9R
+// raban = 17QQ2DIidrb5nXEexj
+// jessica = r1oZImRGo25pXnKqbs
+// gurney = InrV95avsV4gH78IPO
+// chani = TCkYprBuDAgKAWvfIn
+// stilgar = JTJKsyoTwi0a2cxL6P
+// baron harkonnen = kh3PAAIQygXH7AjJDJ
+// irulan = hDZrAfait45NFfL6m1
+// leto = ZmYENgPVGN7tx7dF3K
+// feyd = ymfCaSMMv4FNorPHQ6
+// margot fenring = ZGgZyljBJSOg1zhy2f
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
