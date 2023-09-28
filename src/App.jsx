bd8025f8979c896fd59dd9f02a1c31e1941d1729
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-09-28 17:05:34 -0500

    create app component along with setting initial states for various variables. These variables and setting functions are then passed down the tree via a single object.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,52 @@
 import { useState } from 'react'
 import './App.css'
+import Grid from './components/Grid'
 
 function App() {
   
+  const [currentScore, setCurrentScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [selectionRecord, setSelectionRecord] = useState({
+    paul: false,
+    leto: false,
+    jessica: false,
+    duncan: false,
+    gurney: false,
+    baron: false,
+    raban: false,
+    chani: false,
+    stilgar: false,
+    irulan: false,
+    feyd: false,
+    margot: false
+  });
+
+  const passObject = {
+    cScore: currentScore,
+    bScore: bestScore,
+    cSet: setCurrentScore,
+    bSet: setBestScore,
+    sRecord: selectionRecord,
+    seRecord: setSelectionRecord
+  }
+
+
+
   return (
-    <div>Hello</div>
+    <>
+      <header>
+        <div className="topBar">
+          <h1>Dune Memory Game</h1>
+          <div>
+            <p>Current Score: {currentScore}</p>
+            <p>Best Score: {bestScore}</p>
+          </div>
+        </div>
+        <p>Get points by clicking on an image, but do not click on any more than once!</p>
+      </header>
+      <Grid allInfo={passObject}></Grid>
+    </>
+
   )
 }
 
