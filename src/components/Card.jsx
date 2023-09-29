62fb25aff341ea0028794bff29d82e53f66930bc
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-09-29 15:24:15 -0500

    Modify component so that it is displaying proper names and randomizing the content properly after each click. Add additional styling thru the css file.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,6 +8,21 @@ function Card(props) {
     const cScoreChanger = props.allInfo.cSet;
     const bScore = props.allInfo.bScore;
     const bScoreChanger = props.allInfo.bSet;
+    const cardIndex = Number(props.position);
+    let displayName;
+
+    if (props.orderInfo.name === "Paul") displayName = "Paul Atreides";
+    if (props.orderInfo.name === "Leto") displayName = "Leto Atreides";
+    if (props.orderInfo.name === "Jessica") displayName = "Lady Jessica Of The Atreides";
+    if (props.orderInfo.name === "Duncan") displayName = "Duncan Idaho";
+    if (props.orderInfo.name === "Gurney") displayName = "Gurney Halleck";
+    if (props.orderInfo.name === "Baron") displayName = "Vladimir Harkonnen";
+    if (props.orderInfo.name === "Raban") displayName = "Glossu Rabban";
+    if (props.orderInfo.name === "Chani") displayName = "Chani Kynes";
+    if (props.orderInfo.name === "Stilgar") displayName = "Stilgar of Sietch Tabr";
+    if (props.orderInfo.name === "Irulan") displayName = "Irulan Corrino";
+    if (props.orderInfo.name === "Feyd") displayName = "Feyd-Rautha Harkonnen";
+    if (props.orderInfo.name === "Margot") displayName = "Margot Fenring";
 
     const handleClick = (e) => {
 
@@ -22,10 +37,12 @@ function Card(props) {
                 cScoreChanger(prevState => (prevState + 1));
                 bScoreChanger(nonStateCS);
                 selRecChanger(prevState => ({...prevState, [`${selectedCardInfo}`]: true}));
+                props.card.randomizer(e);
 
             } else {
                 cScoreChanger(prevState => (prevState + 1));
                 selRecChanger(prevState => ({...prevState, [`${selectedCardInfo}`]: true}));
+                props.card.randomizer(e);
 
             }
 
@@ -45,17 +62,17 @@ function Card(props) {
                 feyd: false,
                 margot: false
             });
-
+            props.card.randomizer(e);
         }
 
     }
 
     return (
-        <div className="card" id={props.position} onClick={handleClick} data-name={props.name}>
-            <div className="cardTop" data-topname={props.name}></div>    
-            <div className="cardBottom">
-                {props.name}
-            </div>
+        <div className="card" onClick={handleClick} data-name={props.orderInfo.name}>
+            <img data-topname={props.orderInfo.name} className="cardTop"src={props.orderInfo.image} alt="dune character"/>      
+            <p className="cardBottom">
+                {displayName}
+            </p>
         </div>
     )
 
